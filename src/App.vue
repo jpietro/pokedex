c359dbef631e254ffcb561105ab5d354db30450c
jpietro <pietrojhonatta@gmail.com>
2020-07-11 10:20:40 -0300

    Project Init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Pokemon />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Pokemon from './components/Pokemon.vue';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    Pokemon,
+  },
+};
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style lang="scss">
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
 </style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <h1>Pokedex</h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css?family=Acme');
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    width: calc(100% - 20px);
+    min-height: calc(100vh - 20px);
+    background: radial-gradient(#156F99, #0A2E50);
+    font-family: 'Acme', arial;
+    font-size: 1rem;
+    font-weight: normal;
+  }
+  h1 { color: #efefef; }
+</style>
